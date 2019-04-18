8cb639615205a84e49475ee781f237d0029cf858
MeagMueller <46437198+MeagMueller@users.noreply.github.com>
2019-04-18 11:00:15 -0500

    Starting over

---
+++ src/components/employees/EmployeeList.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+
+export default class EmployeeList extends Component {
+    render() {
+        return (
+            <article>
+                <h1>Employee List</h1>
+                <section>Morticia Addams</section>
+                <section>Rebecca Bunch</section>
+                <section>Seven Opossums in a Trenchcoat</section>
+                <section>A Bog Witch</section>
+            </article>
+        )
+    }
+}
